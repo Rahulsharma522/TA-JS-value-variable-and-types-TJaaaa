64ee094a5022163d7a2109725314ef7cf1707f0c
Rahulsharma522 <sharma.sharma.rahul46@gmail.com>
2021-09-08 13:05:26 +0530

    assign2

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -6,18 +6,32 @@ let lastName = "Stark";
 // Do the following:
 
 // Alert the value of the firstName.
+alert(firstName);
 // Alert the value of the lastName.
+alert(lastName);
 // Assign a new value of `"John"` to firstName.
+let firstName = "john";
 // Assign a new value of `"Snow"` to lastName.
-// Alert the new value of `firstName` and `lastName` by concatenating them
+let lastName = "Snow";
+// Alert the new value of `firstName` and `lastName` by concatenating them 
+alert(firstName+ lastName);
+
 // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
+const fullName= (firstName+ " "+ lastName)
 
 // Declare three variables `name,` `newUser` and `isAdmin`.
-// Assign your name as the value of `name`.
-// Assign the value of `name` to `newUser`.
-// Set the value of `isAdmin` to `true`.
-// Alert the values of all the three variables.
 
+
+// Assign your name as the value of `name`.
+let name= "Rahul";
+// Assign the value of `name` to `newUser`.
+let newUser = "name";
+// Set the value of `isAdmin` to `true`.
+let isAdmin = "true";
+// Alert the values of all the three variables.
+  alert(name);
+  alert(newUser);
+  alert(isAdmin);
 // Assignment and re-assignment
 
 var wiseMan = "Tyrion Lannister";
@@ -25,10 +39,14 @@ var wiseMan = "Tyrion Lannister";
 // Follow instructions and do the following. In case of an error write the error in comment
 
 // Reassign the value of `wiseMan` to "Samwell Tarly"
-// Declare a variable `userName` with value "Lysa Arryn"
-// Declare a variable named `oddNumber` and assign a value `57`.
-// Reassign the value of `oddNumber` to 61
+var wiseMan = "Samwell Tarly";
 
+// Declare a variable `userName` with value "Lysa Arryn"
+let userName = "Lysa Arryn";
+// Declare a variable named `oddNumber` and assign a value `57`.
+let oddNumber = 57;
+// Reassign the value of `oddNumber` to 61
+ oddNumber = 61
 // Mathematical Operation
 
 let amount = 4280;
@@ -36,6 +54,14 @@ let amount = 4280;
 // Using mathematical operations find the solutions. `(+, -, \*, / , etc.)`
 
 // Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
+reducedAmount = 24;
+alert(reducedAmount);
 // Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
+addedAmount = 32;
+alert(addedAmount);
 // Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
+let multipleAmount = 7;
+alert(multipleAmount*multipleAmount);
 // Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
+let dividedAmount = 57;
+alert(dividedAmount/dividedAmount);
